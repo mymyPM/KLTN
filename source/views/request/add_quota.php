8b9a66ff17da639005ff7fc5ae521825ddc819fa
ThienIS <thienis.iuh@gmail.com>
2021-05-15 19:14:33 +0700

    Them add_quota

---
+++ source/views/request/add_quota.php
@@ -0,0 +1,22 @@
+
+		<!-- main content start-->
+		<div id="page-wrapper">
+			<div class="main-page signup-page">
+				<h2 class="title1"></h2>
+				<div class="sign-up-row widget-shadow">
+					<h5>Thêm định mức mới:</h5>
+				<form action="admin.php?mod=request&act=add_quota" method="post">
+					<div class="sign-u">
+							<input type="text" name="txtYear" placeholder="Năm" required="">
+						<div class="clearfix"> </div>
+					</div>
+					<div class="sign-u">
+							<input type="text" name="txtTime" placeholder="Số giờ" required="">
+						<div class="clearfix"> </div>
+					</div>
+					<div class="sub_home">
+							<input type="submit" value="Submit" name="btnRegister">
+						<div class="clearfix"> </div>
+					</div>
+				</form>
+				</div>
