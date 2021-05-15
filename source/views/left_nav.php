3fcda0210db521b6ef743014516c27fdcd8d3853
ThienIS <thienis.iuh@gmail.com>
2021-05-15 18:52:31 +0700

    Fix lef_nav

--- source/views/left_nav.php
+++ source/views/left_nav.php
@@ -17,7 +17,7 @@
             <ul class="sidebar-menu">
               <li class="header">MAIN NAVIGATION</li>
               <li class="treeview">
-                <a href="index.html">
+                <a href="admin.php">
                 <i class="fa fa-dashboard"></i> <span>Dashboard</span>
                 </a>
               </li>
@@ -35,66 +35,41 @@
               </li>
               <li class="treeview">
               <li class="treeview">
-                <a href="#">
+                <a href="admin.php?mod=request&act=quota">
                 <i class="fa fa-laptop"></i>
                 <span>Thiết lập định mức</span>
-                <i class="fa fa-angle-left pull-right"></i>
                 </a>
-                <ul class="treeview-menu">
-                  <li><a href="general.html"><i class="fa fa-angle-right"></i> General</a></li>
-                  <li><a href="icons.html"><i class="fa fa-angle-right"></i> Icons</a></li>
-                  <li><a href="buttons.html"><i class="fa fa-angle-right"></i> Buttons</a></li>
-                  <li><a href="typography.html"><i class="fa fa-angle-right"></i> Typography</a></li>
-                </ul>
               </li>
 			  <li>
                 <a href="admin.php?mod=request&act=list_request">
-                <i class="fa fa-th"></i> <span>Yêu cầu miễn giảm</span>
-                <small class="label pull-right label-info">08</small>
+                <i class="fa fa-th"></i> <span>Thiết lập đợt xét</span>
                 </a>
               </li>
               <li class="treeview">
                 <a href="#">
-                <i class="fa fa-edit"></i> <span>Quản lý document</span>
-                <i class="fa fa-angle-left pull-right"></i>
+                <i class="fa fa-edit"></i> <span>Yêu cầu miễn giảm</span>
                 </a>
-                <ul class="treeview-menu">
-                  <li><a href="forms.html"><i class="fa fa-angle-right"></i> General Forms</a></li>
-                  <li><a href="validation.html"><i class="fa fa-angle-right"></i> Form Validations</a></li>
-                </ul>
               </li>
               <li class="treeview">
                 <a href="#">
                 <i class="fa fa-table"></i> <span>Yêu cầu đã duyệt</span>
-                <i class="fa fa-angle-left pull-right"></i>
                 </a>
-                <ul class="treeview-menu">
-                  <li><a href="tables.html"><i class="fa fa-angle-right"></i> Simple tables</a></li>
-                </ul>
               </li>
               <li class="treeview">
                 <a href="#">
-                <i class="fa fa-envelope"></i> <span>Mailbox </span>
-                <i class="fa fa-angle-left pull-right"></i><small class="label pull-right label-info1">08</small><span class="label label-primary1 pull-right">02</span></a>
-                <ul class="treeview-menu">
-                  <li><a href="inbox.html"><i class="fa fa-angle-right"></i> Mail Inbox </a></li>
-                  <li><a href="compose.html"><i class="fa fa-angle-right"></i> Compose Mail </a></li>
-                </ul>
+                <i class="fa fa-envelope"></i> <span>Quản lý vai trò</span>
+                </a>
               </li>
               <li class="treeview">
                 <a href="#">
-                <i class="fa fa-folder"></i> <span>Examples</span>
-                <i class="fa fa-angle-left pull-right"></i>
+                <i class="fa fa-folder"></i> <span>Quản lý đơn vị</span>
                 </a>
-                <ul class="treeview-menu">
-                  <li><a href="login.html"><i class="fa fa-angle-right"></i> Login</a></li>
-                  <li><a href="signup.html"><i class="fa fa-angle-right"></i> Register</a></li>
-                  <li><a href="404.html"><i class="fa fa-angle-right"></i> 404 Error</a></li>
-                  <li><a href="500.html"><i class="fa fa-angle-right"></i> 500 Error</a></li>
-                  <li><a href="blank-page.html"><i class="fa fa-angle-right"></i> Blank Page</a></li>
-                </ul>
               </li>
-              
+              <li class="treeview">
+                <a href="#">
+                <i class="fa fa-folder"></i> <span>Quản lý loại miễn giảm</span>
+                </a>
+              </li>
             </ul>
           </div>
           <!-- /.navbar-collapse -->
