8d4d306a45de41e8e90bcc3cea14b2ea646b9d5f
ThienIS <thienis.iuh@gmail.com>
2021-05-15 19:11:05 +0700

    Them add_quota

---
+++ source/controllers/request/add_quota.php
@@ -0,0 +1,28 @@
+<?php
+    include_once("source/views/request/add_quota.php");
+    if(isset($_POST["btnRegister"]))
+    {
+        $name = $_POST["txtName"];
+        $email = $_POST["txtEmail"];
+        $group = $_POST["Group"];
+        echo var_dump($group);
+        $pass = $_POST["txtPassword"];
+        $img = $_POST["txtImg"];
+        if($group==="Admin")
+        {
+            $g=1;
+        }
+        else
+        {
+            $g=2;
+        }
+        include_once("source/models/users.php");
+        $user = new Users;
+        $result = $user->createUser($name,$email,$g,$pass,$img);
+        if($result)
+        echo"<script>alert('Thêm account thành công')</script>";
+
+        else
+        echo"<script>alert('Thêm account không thành công')</script>";
+    }
+?>
