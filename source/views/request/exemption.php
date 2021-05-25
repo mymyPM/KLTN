5100ffea83511f9c73b9e729f22ea32badb06d6f
ThienIS <thienis.iuh@gmail.com>
2021-05-26 00:34:57 +0700

    fix KeKhaiLoaiMienGiam

--- source/views/request/exemption.php
+++ source/views/request/exemption.php
@@ -9,38 +9,11 @@ $ds = $exemption->getExemption();
 		<div id="page-wrapper">
 			<div class="main-page">
             <div class="row">
-						<h3 class="title1">Kê khai miễn giảm :
-                            <div style="float:right"><a href="pesonal.php?mod=request&act=exemption&id=<?php echo $row["ID"]; ?>"><i class="fa fa-arrow-circle-left" aria-hidden="true"></i></a></div>
+						<h3 class="title1">Kê khai miễn giảm:
+                            <div style="float:right" hidden><a href="pesonal.php?mod=request&act=exemption&id=<?php echo $row["ID"]; ?>"><i class="fa fa-arrow-circle-left" aria-hidden="true"></i></a></div>
                         </h3>
 						<div class="form-three widget-shadow">
 							<form class="form-horizontal" action="pesonal.php?mod=request&act=exemption&id=<?php echo $id?>" method="post">
-								<div class="form-group">
-									<label for="focusedinput" class="col-sm-2 control-label">Email: </label>
-									<div class="col-sm-8">
-										<input type="text" class="form-control1" id="focusedinput" placeholder="Default Input">
-									</div>
-									<div class="col-sm-2">
-										<p class="help-block">Your help text!</p>
-									</div>
-                                </div>
-                                <div class="form-group">
-									<label for="focusedinput" class="col-sm-2 control-label">Name: </label>
-									<div class="col-sm-8">
-										<input type="text" class="form-control1" id="focusedinput" placeholder="Default Input">
-									</div>
-									<div class="col-sm-2">
-										<p class="help-block">Your help text!</p>
-									</div>
-                                </div>
-                                <div class="form-group">
-									<label for="focusedinput" class="col-sm-2 control-label">Faculty: </label>
-									<div class="col-sm-8">
-										<input type="text" class="form-control1" id="focusedinput" placeholder="Default Input">
-									</div>
-									<div class="col-sm-2">
-										<p class="help-block">Your help text!</p>
-									</div>
-								</div>
 								<div class="form-group">
 									<label for="selector1" class="col-sm-2 control-label">Loại miễn giảm:</label>
 									<div class="col-sm-8">
@@ -69,20 +42,7 @@ $ds = $exemption->getExemption();
 									?>
 										<input type="text" class="form-control1" id="focusedinput" placeholder="Default Input" value="<?php echo $n  ?>">
 									</div>
-									<div class="col-sm-2">
-										<p class="help-block">Your help text!</p>
-									</div>
-								</div>
-								<div class="form-group">
-									<label for="txtarea1" class="col-sm-2 control-label">Textarea:</label>
-									<div class="col-sm-8"><textarea name="txtarea1" id="txtarea1" cols="50" rows="4" class="form-control1"></textarea></div>
-                                </div>
-                                <div class="form-group"> 
-                                    <label for="exampleInputFile" class="col-sm-2 control-label">File input:</label> 
-                                    <input type="file" id="exampleInputFile" class="col-sm-8"> 
-                                    <p class="help-block">Example block-level help text here.</p> 
-                                </div> 
-                                 
+								</div>                                 
                                 <input type="submit" value="Submit" name="btnExemption" class="btn btn-default" class="col-sm-2 control-label">
 								
 								
