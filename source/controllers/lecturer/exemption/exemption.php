db4b5809738a9bc624356fadca63b08b7a9eab50
ThienIS <thienis.iuh@gmail.com>
2021-05-27 16:56:24 +0700

    fix exemption, notice

--- source/controllers/lecturer/exemption/exemption.php
+++ source/controllers/lecturer/exemption/exemption.php
@@ -1,10 +1,22 @@
 <?php
 include_once("source/views/request/exemption.php");
-include_once("source/models/exemption.php");
-$exemption = new Exemption();
 
 if(isset($_POST["btnExemption"]))
 {
-
+    $lecturer = $_SESSION["LecturerID"];
+    $exemp = $_POST["txtLoai"];
+    $exemption_type = $exemption->getExemptiontypeByName($exemp);
+    include_once("source/models/exemption.php");
+    $exemption = new Exemption();
+    $result = $exemption->addExemption($lecturer,$exemption_type);
+    echo var_dump($lecturer,$exemption_type,$exemp);
+    if($result)
+    {
+        echo"<script>alert('Add request successfully!!!')</script>";
+    }
+    else
+    {
+        echo"<script>alert('Error')</script>";
+    }
 }
 ?>
